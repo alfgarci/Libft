bb3854fa583fd1410398eed5cc7c991505cf9cbb
Alfonso Garcia <alf8ns8@gmail.com>
2022-08-09 18:42:58 +0200

    fixed a part of ft_split

--- src/ft_split.c
+++ src/ft_split.c
@@ -6,22 +6,29 @@
 /*   By: alfgarci <alfgarci@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 12:27:49 by alfgarci          #+#    #+#             */
-/*   Updated: 2022/08/09 16:33:34 by alfgarci         ###   ########.fr       */
+/*   Updated: 2022/08/09 18:40:57 by alfgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_chrs(const char *s, char c)
+static char	*get_word(const char *s, char c)
 {
-	int	i;
-	int	count;
+	int		i;
+	int		count;
+	char	*tmp;
 
 	i = -1;
 	count = 0;
 	while (s[++i] != c)
 		count++;
-	return (count);
+	tmp = (char *)malloc((count + 1) * sizeof(char));
+	if (!tmp)
+		return (NULL);
+	i = -1;
+	while (s[++i] != c)
+		tmp[i] = s[i];
+	return (tmp);
 }
 
 static int	count_strs(const char *s, char c)
@@ -31,10 +38,12 @@ static int	count_strs(const char *s, char c)
 
 	i = -1;
 	count = 0;
+	if (s[0] != c)
+		count++;
 	while (s[++i])
-		if (s[i] == c)
+		if (s[i] == c && s[i + 1] && s[i + 1] != c)
 			count++;
-	return (count + 1);
+	return (count);
 }
 
 char	**ft_split(const char *s, char c)
@@ -42,20 +51,34 @@ char	**ft_split(const char *s, char c)
 	char	**arr_s;
 	int		i;
 	int		j;
-	int		k;
 
 	i = -1;
 	j = -1;
-	k = -1;
 	arr_s = (char **)malloc((count_strs(s, c) + 1) * sizeof(char *));
+	if (!arr_s)
+		return (NULL);
+	if (s[0] != c)
+		arr_s[++j] = get_word((s + i + 1), c);
 	while (s[++i] != '\0')
-	{
-		arr_s[++j] = (char *)malloc((count_chrs(s + i, c) + 1) * sizeof(char));
-		while (s[i] != c)
-			arr_s[j][++k] = s[i++];
-		arr_s[j][++k] = '\0';
-		k = -1;
-	}
+		if (s[i] == c && s[i + 1] && s[i + 1] != c)
+			arr_s[++j] = get_word((s + i + 1), c);
 	arr_s[++j] = NULL;
 	return (arr_s);
 }
+/*
+int	main(int ac, char **av)
+{
+	char	*to_sp;
+	char	sep;
+	char	**spp;
+
+	if (ac == 3)
+	{
+		to_sp = av[1];
+		sep = av[2][0];
+		spp = ft_split(to_sp, sep);
+		for (int i = 0; i < count_strs(to_sp, sep); i++)
+			printf("Cad %d -> $%s$\n", i, spp[i]);
+	}
+	return (0);
+}*/
