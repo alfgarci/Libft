23a239735dc22e1873dd7a9409c5336dd0d776d5
Alfonso Garcia <alf8ns8@gmail.com>
2022-08-11 11:01:46 +0200

    fixed strmapi

--- src/ft_strmapi.c
+++ src/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: alfgarci <alfgarci@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 10:47:50 by alfgarci          #+#    #+#             */
-/*   Updated: 2022/08/11 10:58:20 by alfgarci         ###   ########.fr       */
+/*   Updated: 2022/08/11 11:01:22 by alfgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,13 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 	char	*cad;
 
 	i = -1;
+	if (!s)
+		return (NULL);
 	cad = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (!cad)
 		return (NULL);
 	while (s[++i])
 		cad[i] = f(i, s[i]);
+	cad[i] = '\0';
 	return (cad);
 }
