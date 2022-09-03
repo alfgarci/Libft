b3a26ca5b8128ef15da85472fb188d625aec2e68
Alfonso Garcia <alf8ns8@gmail.com>
2022-09-03 18:47:32 +0200

    ft_putnbr_fd.c fixed

--- src/ft_putnbr_fd.c
+++ src/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: alfgarci <alfgarci@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 16:50:15 by alfgarci          #+#    #+#             */
-/*   Updated: 2022/08/11 16:54:51 by alfgarci         ###   ########.fr       */
+/*   Updated: 2022/09/01 13:02:19 by alfgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,9 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	char	*num;
+
+	num = ft_itoa(n);
 	ft_putstr_fd(ft_itoa(n), fd);
+	free(num);
 }
